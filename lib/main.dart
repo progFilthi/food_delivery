83763db531de237dc04834ad4c66041820378894
ProdFilthi <gamerfilthi@gmail.com>
2025-07-21 18:37:03 +0200

    This is a working flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,12 @@ import 'package:food_delivery_app/screens/home/item_details_screen.dart';
 import 'package:food_delivery_app/screens/orders/orders_screen.dart';
 import 'package:food_delivery_app/screens/orders/order_status_screen.dart';
 import 'package:food_delivery_app/screens/orders/order_summary_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
